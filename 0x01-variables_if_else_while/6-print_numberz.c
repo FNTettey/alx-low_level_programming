62f83ea00dde889e5dfe5912208be9372df27ac5
FNTettey <nii4tettey@gmail.com>
2022-03-11 19:20:07 -0800

    Counting

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ *main - entry point
+ *Description:  all single digit numbers of base 10 starting from 0
+ *Return: Always return zero(0)
+ **/
+int main(void)
+{
+int x;
+ /* My code */
+for (x = 0; x <= 9; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
