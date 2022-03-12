3f303a0df30a9be29bbe10ecf91648cc9cd39027
FNTettey <nii4tettey@gmail.com>
2022-03-11 19:34:13 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+/**
+ *prints all single digit numbers of base 10 starting from 0
+ **/
+int main(void)
+{
+printf("123456789\n");
+return (0);
+}
