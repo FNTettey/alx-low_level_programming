21b13dd1def74c6fc4f9fb2020c106698bd3613b
FNTettey <nii4tettey@gmail.com>
2022-03-12 23:07:52 -0800

    Hex

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,28 +3,22 @@
 #include<time.h>
 #include<stdio.h>
 /**
-
  *main - entry point
-
  *Description: Print all base 16 numbers in lower case.
-
  *Return: Always return zero(0)
-
  **/
 int main(void)
 {
 int x;
   /* My code */
-for (x = 0; x <= 15; x++)
-{
-if (x < 10)
+for (x = 0; x <= 9; x++)
 {
 putchar(x);
 }
-else
+for (x = 'a'; x <= 'f'; x++)
 {
-putchar("%x", &x);
-}
+x = tolower(x);
+putchar(x);
 }
 putchar('\n');
 return (0);
