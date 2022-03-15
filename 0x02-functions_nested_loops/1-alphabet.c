91b3301ae4f8d9b3ce426835009296cc2b0dcfd7
FNTettey <nii4tettey@gmail.com>
2022-03-15 03:45:39 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - check the code
+ * description: Prints the alphabet in lowercase
+ * Return: Always 0.
+ */
+int main(void)
+{
+int print_alphabet(void)
+{
+int n;
+for (x = 'A'; x <= 'z'; x++)
+{
+x = tolower(x);
+putchar(x);
+}
+putchar("\n")
+return (0);
+}
