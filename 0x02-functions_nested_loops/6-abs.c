d8362ca64d450f8d2ab830b77180e8c0431fe3f8
FNTettey <nii4tettey@gmail.com>
2022-03-15 14:08:03 -0700

    Absoloute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ *main - entry point
+ *Description: Prints absolute value of a number
+ *Return: Always return zero(0)
+ **/
+int _abs(int n)
+{
+_putchar(abs(n));
+return (0);
+}
