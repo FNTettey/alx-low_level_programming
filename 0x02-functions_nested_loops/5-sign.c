fe6b4601b0c55742415b8a9342b5e32b399aaca0
FNTettey <nii4tettey@gmail.com>
2022-03-15 12:39:23 -0700

    Pos or Neg?

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,15 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return ("+1");
+return (+1);
 }
 else if (n == 0)
 {
-return ("00");
+return (00);
 }
 else
 {
-return ("-1");
+return (-1);
 }
 return (0);
 }
