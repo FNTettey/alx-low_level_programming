206c17fff21a1c8967311f2624eff94ae6d976f9
FNTettey <nii4tettey@gmail.com>
2022-03-15 09:11:29 -0700

    Lowercase?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * main - check the code
+ * description: Prints the alphabet in lowercase
+ * Return: Always 0.
+ */
+int _islower(int c)  
+/*description: Prints the alphabet in lowercase */
+{
+if (islower(c) != 0)
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
