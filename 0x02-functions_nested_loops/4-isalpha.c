ffc0e80826df40d4615ca1f98fd24d7483f6809e
FNTettey <nii4tettey@gmail.com>
2022-03-15 09:40:48 -0700

    Letter?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * main - check the code
+ * description: Prints the alphabet in lowercase
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+/**
+ * description: Prints the alphabet in lowercase
+ * Parameter description: a character inputed by the user
+ **/
+{
+if (isalpha(c) != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
