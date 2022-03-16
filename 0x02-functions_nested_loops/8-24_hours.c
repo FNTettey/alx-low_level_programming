f9da9f061306ee32f8efb5c9d8eb3041ff2aafd4
FNTettey <nii4tettey@gmail.com>
2022-03-15 18:44:10 -0700

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ *main - entry point
+ *Description: prints every minute of the day
+ *Return: Always return zero
+ **/
+int j;
+int k;
+int l;
+int m;
+void jack_bauer(void)
+{
+for (j = 0; j <= 2; j++);
+ {
+for (k = 0; k <= 4; k++);
+ {
+for (l = 0; l <= 6; l++);
+ {
+for (m = 0; m <= 9; m++);
+ {
+printf("%d%d:%d%d\n",j,k,l,m);
+ }
+  }
+  }                                                                      
+   }
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 #include<math.h>
+void jack_bauer(void);
