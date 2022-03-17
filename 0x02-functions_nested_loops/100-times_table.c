f28c1d47270ea688cef8991f1eb3c4c961ae578d
FNTettey <nii4tettey@gmail.com>
2022-03-16 17:29:50 -0700

    n times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ *main - entry point
+ *Description: prints the n times tables.
+ *Return: Always return zero
+ *@n the value the user enters
+ **/
+void print_times_table(int n)
+{
+if (n > 0 && n < 16)
+{
+int p;
+for (n = 0; n <= 9; n++)
+{
+for (n = 0; n <= 9; n++)
+{
+p = (n * n);
+putchar(p + '0');
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+}
