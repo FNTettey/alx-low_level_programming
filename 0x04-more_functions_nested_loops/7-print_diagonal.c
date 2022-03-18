76bb470089e990beace7b51a9979da6a74042ca2
FNTettey <nii4tettey@gmail.com>
2022-03-17 19:00:27 -0700

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include"main.h"
+/**
+ *main - entry point
+ *Description: prints a line using "_" n times
+ *Return: Always return zero
+ *@n the value the user enters
+ **/
+void print_diagonal(int n)
+{
+if (n > 0)
+{
+int j;
+int s;
+s = 0;
+j = 0;
+while (j <= n)
+{
+int k;
+k = 0;
+if (s > k)
+{
+for (k = 0; k <= s; k++)
+{
+_putchar(' ');
+}
+}
+_putchar('\\');
+s++;
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
+
