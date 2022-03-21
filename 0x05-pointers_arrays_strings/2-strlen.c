f7082d4a4d8639afb722c2bc5977a7a7d2191bd0
FNTettey <nii4tettey@gmail.com>
2022-03-21 12:49:22 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _strlen() - Entry point
+ * description: returns the length of a string
+ * @s points to the string we want to find the length of
+ **/
+int _strlen(char *s)
+{
+int length;
+length = strlen(s);
+return (length);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,7 @@
 #include<ctype.h>
 #include <stdlib.h>
 #include<math.h>
+#include<string.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
