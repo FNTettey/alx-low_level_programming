15041da4c270582ebd3b3192bbaa617897994d25
FNTettey <nii4tettey@gmail.com>
2022-03-21 06:44:48 -0700

    change value at pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include"main.h"
+/**
+ * reset_to_98() - Entry point
+ * description: updates the value *n points to to 98.
+ * @n points the the variable being updated
+ **/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#include <stdio.h>
+#include<ctype.h>
+#include <stdlib.h>
+#include<math.h>
+void reset_to_98(int *n);
