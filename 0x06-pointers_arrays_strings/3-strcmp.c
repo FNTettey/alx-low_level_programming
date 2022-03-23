87bc5b81119a88eac2daf64a8b15abc7980c4014
FNTettey <nii4tettey@gmail.com>
2022-03-23 15:54:32 -0700

    Comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _strcmp - 0, if the s1 = s2 .a negative value if s1 < s2
+ * a positive value if s1 > s2.
+ * @s1 first string
+ * @s2 second string
+ * Return: A positive number, a negative number or zero
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
