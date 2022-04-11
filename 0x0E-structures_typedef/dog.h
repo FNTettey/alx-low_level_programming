41c23d3ec5e7869839dd60c80182b872d4513f71
FNTettey <nii4tettey@gmail.com>
2022-04-11 15:36:21 -0700

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,39 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+int _putchar(char c);
+
+/**
+
+ *struct dog - Initialize struct dog
+
+ *@name:name type char
+
+ *@age: age type float
+
+ *@owner: name of owner tyme char
+
+ *
+
+ *Description: name, age and owner of a dog
+
+ */
+
+struct dog
+
+{
+  
+  char* name;
+  
+  float age;
+  
+  char* owner;
+  
+} dog;
+
+
+
+
+
+#endif /* DOG_H */
