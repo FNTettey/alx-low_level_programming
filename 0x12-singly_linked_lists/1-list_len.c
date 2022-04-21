931e9bb2d9c9e847ccbc91d5298a6f91dab41946
FNTettey <nii4tettey@gmail.com>
2022-04-21 14:42:54 -0700

    Length of a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - returns the number of elements in a linked list
+ * @h: first element of the list
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+int i;
+i = 0;
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
+}
