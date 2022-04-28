50d9f3d4d845066013a16dbb1a6992a978f071ee
FNTettey <nii4tettey@gmail.com>
2022-04-28 10:12:04 -0700

    Setting specific bit to 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,5 @@ unsigned int _pow(unsigned int num, unsigned int n);
 #include <stdio.h>
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
