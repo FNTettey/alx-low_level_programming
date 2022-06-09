9a73a0f8c735f502727215b3f2dd0cce7c01288d
=Nii <=nii4tettey@gmail.com>
2022-06-09 19:55:55 +0200

    printing all elements in a doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
- 
 /**
  * print_dlistint - print elements of a  dlistint_t list
  * @h: first element of the list
@@ -9,14 +8,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
- 
-	while (h != NULL)
-	{
-		printf("%d", h->n);
-		i++;
-		h = h->next;
-		printf("\n");
-	}
-	return (i);
+size_t i = 0;
+while (h != NULL)
+{
+printf("%d", h->n);
+i++;
+h = h->next;
+printf("\n");
+}
+return (i);
 }
