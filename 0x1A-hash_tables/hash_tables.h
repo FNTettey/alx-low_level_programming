7da2f9c8951c9a19930cd1838445d68354d20417
=Nii <=nii4tettey@gmail.com>
2022-07-01 01:43:43 +0200

    deleting a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -44,5 +44,5 @@ int set_pair_only(hash_table_t *ht, const char *key, const char *value,
 		  unsigned long int index);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
-
+void hash_table_delete(hash_table_t *ht);
 #endif
